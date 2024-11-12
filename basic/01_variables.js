36c9f7dff3123fdf0355ac3a8de4550841c1485e
Manan Mishra <137518804+Mananmishra2004@users.noreply.github.com>
2024-11-12 08:31:45 +0000

    01_variables

---
+++ basic/01_variables.js
@@ -0,0 +1,21 @@
+//let const var are the type of variables 
+// let is use for of we want to change the value after the dicleration so it is possible by the let 
+
+// for example
+let username = "manan"
+console.log(username );
+username = "ram" ;
+console.log(username );
+
+// const is use for decleration as value which can't be change after a decleration
+
+// for example 
+const username2 = "raman";
+console.log(username2);
+// username2 ="chaman"; //error type is constant variable which can't be change after a decleration 
+console.log(username2);
+
+
+//var is use as a veriable but it have a major drowback it can't justify the scope in mordern js we are not use the var
+
+
